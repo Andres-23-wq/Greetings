3766537d43c5957018b09cd5c464074bd5f3f1fe
OscarAndres <oscarandresgonzalezbeltran@gmail.com>
2024-03-25 16:09:22 +0100

    Add files via upload

---
+++ frontend/greeting.js
@@ -0,0 +1,9 @@
+function submitName() {
+    const enteredName = document.getElementById('nameInput').value;
+    const greetingMessage = `Hello, ${enteredName}!`;
+    updateUI(greetingMessage);
+}
+
+function updateUI(message) {
+    document.getElementById('greetingMessage').innerText = message;
+}
