cf82fe4edc5b42d2c3c9749c0c009bd26d34fb64
OscarAndres <oscarandresgonzalezbeltran@gmail.com>
2024-03-25 16:10:32 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+let app = express();
+app.use(express.static('frontend'));
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
